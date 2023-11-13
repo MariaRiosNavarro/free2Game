7a0f6fbf3ee343f61787f65f6fce7ed78ddd31ea
SuperCoder <propscode254@gmail.com>
2023-11-13 16:07:24 +0100

    RecAdd works

--- src/components/RecAdd/RecAdd.jsx
+++ src/components/RecAdd/RecAdd.jsx
@@ -1,10 +1,49 @@
 import "./RecAdd.css";
+import Card from "../Card/Card";
+import { useEffect, useState } from "react";
 
 const RecAdd = () => {
+  const [newGames, setNewGames] = useState([]);
+
+  const url =
+    "https://free-to-play-games-database.p.rapidapi.com/api/games?sort_by=release-date";
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "80caff7fbemshc112244508e4c65p1241abjsnd55fb8f398b4",
+      "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+    },
+  };
+
+  useEffect(() => {
+    fetch(url, options)
+      .then((response) => response.json())
+      .then((games) => setNewGames(games))
+      .catch((err) => console.error("Fetch Isue", err));
+  }, []);
+
+  console.log(newGames);
+
   return (
-    <div>
-      <h2>RecAdd</h2>
-    </div>
+    <section>
+      {newGames ? (
+        newGames.map((item, index) => (
+          <Card
+            key={index}
+            title={item.title}
+            thumbnail={item.thumbnail}
+            description={true}
+            short_description={item.short_description}
+            platform={item.platform}
+            genre={item.genre}
+            id={item.id}
+          />
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
+    </section>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,9 @@
+import RecAdd from "../components/RecAdd/RecAdd";
+
 const HomePage = () => {
   return (
     <div>
+      <RecAdd />
       <h2>HomePage</h2>
     </div>
   );
