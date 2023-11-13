c140d0ccc044de71c1c94de4bb23357633feb9e3
SuperCoder <propscode254@gmail.com>
2023-11-13 13:47:32 +0100

    add card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,9 +2,25 @@ import "./Card.css";
 
 const Card = () => {
   return (
-    <div>
-      <h2>Card</h2>
-    </div>
+    <article>
+      <div className="img-wrapper">
+        <img src={props.thumbnail} alt={props.title} />
+      </div>
+      <div className="text-wrapper">
+        <h3 className="title">{props.title}</h3>
+        {props.description ? (
+          <p className="description">{props.short_description}</p>
+        ) : null}
+      </div>
+      <Link to={`/detail/${props.id}`}>
+        <button>READ MORE</button>
+      </Link>
+      <hr className="separation-line" />
+      <div className="badge-wrapper">
+        <span className="badge">{props.platform}</span>
+        <span className="badge">{props.genre}</span>
+      </div>
+    </article>
   );
 };
 
