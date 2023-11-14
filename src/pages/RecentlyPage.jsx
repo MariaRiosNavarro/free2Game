1f32b62b995b79d2e0c557b52bc2255532886db4
SuperCoder <propscode254@gmail.com>
2023-11-14 09:34:31 +0100

    add header everywhere

--- src/pages/RecentlyPage.jsx
+++ src/pages/RecentlyPage.jsx
@@ -1,10 +1,8 @@
-import Header from "../components/Header/Header";
-
 const RecentlyPage = () => {
   return (
     // ! Nur zum testen eine höhe gegeben damit ich sehen kann ob der header sticky bleibt
     <div style={{ height: "150vh", boxSizing: "border-box" }}>
-      <Header />
+      {/* <Header /> */}
     </div>
   );
 };
