4c618c091f88712831ed3bdb457ab95ea9ce20e2
Andre Wolf <Andre77242@hotmail.de>
2023-11-14 14:25:26 +0100

    fn for inputHandleChange

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -9,6 +9,13 @@ const Category = ({ genres }) => {
   const [selectedGenre, setSelectedGenre] = useState("GENRE/TAG");
   const [sortBy, setSortBy] = useState("SORT BY");
 
+  // Zustand, um die ausgewählten Optionen zu speichern
+  const [selectedFilters, setSelectedFilters] = useState({
+    platform: [],
+    genre: [],
+    sortBy: [],
+  });
+
   const toggleMenuPlatform = () => {
     setIsOpenPlatform(!isOpenPlatform);
   };
@@ -16,11 +23,30 @@ const Category = ({ genres }) => {
   const toggleMenuGenre = () => {
     setIsOpenGenre(!isOpenGenre);
   };
+
   const toggleSortBy = () => {
     setIsOpenSortBy(!isOpenSortBy);
   };
 
-  // const copyArray = [...newGames];
+  const handleCheckboxChange = (category, value) => {
+    // Kopiere das aktuelle ausgewählte Filterset
+    const newSelectedFilters = { ...selectedFilters };
+
+    // Überprüfe, ob der Wert bereits ausgewählt ist
+    const index = newSelectedFilters[category].indexOf(value);
+
+    if (index !== -1) {
+      // Wenn ausgewählt, entferne den Wert
+      newSelectedFilters[category].splice(index, 1);
+    } else {
+      // Wenn nicht ausgewählt, füge den Wert hinzu
+      newSelectedFilters[category].push(value);
+    }
+
+    // Aktualisiere den Zustand mit den neuen ausgewählten Filtern
+    setSelectedFilters(newSelectedFilters);
+    console.log("Selected Filters:", selectedFilters);
+  };
 
   return (
     <div className="categoryContainer">
@@ -35,19 +61,45 @@ const Category = ({ genres }) => {
         {isOpenPlatform && (
           <form action="" className="Form">
             <div>
-              <input type="checkbox" name="allPlatforms" id="allPlatforms" />
+              <input
+                type="checkbox"
+                value="allPlatforms"
+                name="allPlatforms"
+                id="allPlatforms"
+                onChange={() =>
+                  handleCheckboxChange("platform", "allPlatforms")
+                }
+              />
               <label htmlFor="allPlatforms">all Platforms</label>
             </div>
             <div>
-              <input type="checkbox" name="windows" id="windows" />
+              <input
+                type="checkbox"
+                value="windows"
+                name="windows"
+                id="windows"
+                onChange={() => handleCheckboxChange("platform", "windows")}
+              />
               <label htmlFor="windows">Windows</label>
             </div>
             <div>
-              <input type="checkbox" name="browser" id="browser" />
+              <input
+                type="checkbox"
+                value="browser"
+                name="browser"
+                id="browser"
+                onChange={() => handleCheckboxChange("platform", "browser")}
+              />
               <label htmlFor="browser">Browser</label>
             </div>
           </form>
         )}
+        <div>
+          {" "}
+          {selectedFilters.platform.length === 0
+            ? "Platforms"
+            : selectedFilters.platform.join(", ")}
+        </div>
       </div>
       <div className="custom-dropdown">
         <div
@@ -60,15 +112,32 @@ const Category = ({ genres }) => {
         {isOpenGenre && (
           <form action="" className="Form">
             <div>
-              <input type="checkbox" name="shooter" id="shooter" />
+              <input
+                type="checkbox"
+                value="shooter"
+                name="shooter"
+                id="shooter"
+                onChange={() => handleCheckboxChange("genre", "shooter")}
+              />
               <label htmlFor="shooter">shooter</label>
             </div>
             <div>
-              <input type="checkbox" name="sci-fi" id="sci-fi" />
+              <input
+                type="checkbox"
+                value="sci-fi"
+                name="sci-fi"
+                id="sci-fi"
+                onChange={() => handleCheckboxChange("genre", "sci-fi")}
+              />
               <label htmlFor="sci-fi">sci-fi</label>
             </div>
           </form>
         )}
+        <div>
+          {selectedFilters.genre.length === 0
+            ? "Genre"
+            : selectedFilters.genre.join(", ")}
+        </div>
       </div>
       <div className="custom-dropdown">
         <div
@@ -81,23 +150,48 @@ const Category = ({ genres }) => {
         {isOpenSortBy && (
           <form action="" className="Form">
             <div>
-              <input type="checkbox" name="relevance" id="relevance" />
+              <input
+                type="checkbox"
+                name="relevance"
+                id="relevance"
+                onChange={() => handleCheckboxChange("sortBy", "relevance")}
+              />
               <label htmlFor="relevance">Relevance</label>
             </div>
             <div>
-              <input type="checkbox" name="popularity" id="popularity" />
+              <input
+                type="checkbox"
+                name="popularity"
+                id="popularity"
+                onChange={() => handleCheckboxChange("sortBy", "popularity")}
+              />
               <label htmlFor="popularity">Popularity</label>
             </div>
             <div>
-              <input type="checkbox" name="releaseDate" id="releaseDate" />
+              <input
+                type="checkbox"
+                name="releaseDate"
+                id="releaseDate"
+                onChange={() => handleCheckboxChange("sortBy", "releaseDate")}
+              />
               <label htmlFor="releaseDate">Release Date</label>
             </div>
             <div>
-              <input type="checkbox" name="alphabetical" id="alphabetical" />
+              <input
+                type="checkbox"
+                name="alphabetical"
+                id="alphabetical"
+                onChange={() => handleCheckboxChange("sortBy", "alphabetical")}
+              />
               <label htmlFor="alphabetical">Alphabetical</label>
             </div>
           </form>
         )}
+        <div>
+          {selectedFilters.sortBy.length === 0
+            ? "SortBy"
+            : selectedFilters.sortBy.join(", ")}
+        </div>
       </div>
     </div>
   );
