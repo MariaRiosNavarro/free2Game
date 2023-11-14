3160642b04239774cb45567709164eec0d3e7767
Yan Luka Keita <yanlukakeita2000@gmail.com>
2023-11-14 15:33:08 +0100

    Filter categoryies etc

--- src/pages/AllGamesPage.jsx
+++ src/pages/AllGamesPage.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Category from "../components/Category/Category";
 import Gallery from "../components/Gallery/Gallery";
 
 const AllGamesPage = ({ newGames }) => {
+  const [filteredGames, setGames] = useState(newGames);
+
   return (
     <div>
       <h2>AllGamesPage</h2>
-      <Category newGames={newGames} />
-      <Gallery newGames={newGames} />
+      <Category setGames={setGames} />
+      <Gallery newGames={filteredGames} />
     </div>
   );
 };
