6f2df1ca07b267467c8ad67cb51e2d1c4a3ac909
Andre Wolf <Andre77242@hotmail.de>
2023-11-14 10:15:45 +0100

    category custom-dropdown-menus

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,9 +1,57 @@
+import React, { useState } from "react";
 import "./Category.css";
 
-const Category = () => {
+const Category = ({ genres }) => {
+  const [isOpenPlatform, setIsOpenPlatform] = useState(false);
+  const [isOpenGenre, setIsOpenGenre] = useState(false);
+  const [selectedPlatform, setSelectedPlatform] = useState("PLATFORM");
+  const [selectedGenre, setSelectedGenre] = useState("GENRE/TAG");
+
+  const toggleMenuPlatform = () => {
+    setIsOpenPlatform(!isOpenPlatform);
+  };
+
+  const toggleMenuGenre = () => {
+    setIsOpenGenre(!isOpenGenre);
+  };
+
   return (
     <div>
       <h2>Category</h2>
+      <div className="custom-dropdown">
+        <div
+          className="selected-Platform"
+          onClick={toggleMenuPlatform}
+          onBlur={() => setIsOpenPlatform(false)}
+        >
+          {selectedPlatform === "platforms" ? "Platforms" : selectedPlatform}
+        </div>
+        {isOpenPlatform && (
+          <form action="">
+            <label htmlFor="windows">Windows</label>
+            <input type="checkbox" name="windows" id="windows" />
+            <label htmlFor="browser">Browser</label>
+            <input type="checkbox" name="browser" id="browser" />
+          </form>
+        )}
+      </div>
+      <div className="custom-dropdown">
+        <div
+          className="selected-Genre"
+          onClick={toggleMenuGenre}
+          onBlur={() => setIsOpenGenre(false)}
+        >
+          {selectedGenre === "genre" ? "Genre" : selectedGenre}
+        </div>
+        {isOpenGenre && (
+          <form action="" className="GenreInputs">
+            <label htmlFor="shooter">shooter</label>
+            <input type="checkbox" name="shooter" id="shooter" />
+            <label htmlFor="sci-fi">sci-fi</label>
+            <input type="checkbox" name="sci-fi" id="sci-fi" />
+          </form>
+        )}
+      </div>
     </div>
   );
 };
