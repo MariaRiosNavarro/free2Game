a265d180950cc6b71b29c72114559b8cc0d877b4
SuperCoder <propscode254@gmail.com>
2023-11-15 09:17:00 +0100

    done

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,8 @@ import "./Card.css";
 import Button from "../Button/Button";
 import Pc from "../../assets/img/PC.svg";
 import Browser from "../../assets/img/Browser.svg";
+import PCSvg from "../PCSvg/PCSvg";
+import BrowserSvg from "../BrowserSVG/BrowserSvg";
 
 const Card = (props) => {
   return (
@@ -11,18 +13,14 @@ const Card = (props) => {
       </div>
       <div className="text-wrapper">
         <h3 className="title">{props.item.title}</h3>
-        {props.item.description ? (
+        {props.description ? (
           <p className="description">{props.item.short_description}</p>
         ) : null}
         <Button href={`/detail/${props.item.id}`} btnName="READ MORE" />
         <hr className="separation-line" />
         <div className="badge-wrapper">
           <span className="badge badge-img-wrapper">
-            <img
-              src={props.item.platform === "Web Browser" ? Browser : Pc}
-              alt={props.item.title}
-            />
-            {console.log(props.item.platform)}
+            {props.item.platform === "Web Browser" ? <BrowserSvg /> : <PCSvg />}
           </span>
           <span className="badge bagge-genre">{props.item.genre}</span>
         </div>
